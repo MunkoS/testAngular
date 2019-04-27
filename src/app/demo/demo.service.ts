735e31afb72f042a57304602e7831021565bfdc4
MunkoS <Munko_s@mail.ru>
2019-04-27 12:53:46 +0600

    demo test

---
+++ src/app/demo/demo.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {delay} from 'rxjs/operators';
+import {of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DemoService {
+
+  protected value = 'real value';
+
+  getValue() { return this.value; }
+  setValue(value: string) { this.value = value; }
+
+  getObservableValue() { return of('observable value'); }
+
+  getPromiseValue() { return Promise.resolve('promise value'); }
+
+  getObservableDelayValue() {
+    return of('observable delay value').pipe(delay(10));
+  }
+}
